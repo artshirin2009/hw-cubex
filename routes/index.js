05f7207e74e1f1e261826dc0622fe6f6cfba9cf1
Arthur Shirin <artshirin2009@gmail.com>
2019-01-05 17:11:09 +0200

    products page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-
+var Product = require("../models/products");
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+router.get("/", function(req, res, next) {
+  Product.find().then(function(doc) {
+    res.render("shop/index", {
+      products: doc
+    });
+  });
 });
 
 module.exports = router;
